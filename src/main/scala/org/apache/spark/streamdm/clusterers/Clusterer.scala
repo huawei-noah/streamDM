0ea7a594631c18628322c2a262332e58742a1222
Silviu Maniu <silviu.maniu@gmail.com>
2015-04-14 15:06:05 +0800

    [ISSUE-17] Initial clustering hierarchy.

---
+++ src/main/scala/org/apache/spark/streamdm/clusterers/Clusterer.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015 Holmes Team at HUAWEI Noah's Ark Lab.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.spark.streamdm.clusterers
+
+import com.github.javacliparser.Configurable
+import org.apache.spark.streamdm.core._
+import org.apache.spark.streaming.dstream._
+
+/**
+ * A Clusterer trait defines the needed operations for any implemented
+ * clustering algorithm. It provides methods for clustering and for returning
+ * the computed cluster.
+ */
+trait Clusterer extends Configurable  with Serializable {
+
+  /* Init the underlying cluster data structure
+   */
+  def init: Unit
+
+  /* Cluster a stream of input examples
+   * @param input a DStream containing Example
+   * */
+  def cluster(input: DStream[Example]): Unit
+
+  /* Get the currently computed clusters
+   * @return an Array of Examples representing the clusters
+   */
+  def getClusters: Array[Example]
+}
---
+++ src/main/scala/org/apache/spark/streamdm/clusterers/clusters/Clusters.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 Holmes Team at HUAWEI Noah's Ark Lab.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.spark.streamdm.clusterers.clusters
+
+import org.apache.spark.streamdm.core._
+
+
+/**
+ * A Clusters trait defines the needed operations for maintaining a clustering
+ * data structure. It mainly provides a method to update the data structure
+ * based on an Instance.
+ */
+trait Clusters extends Serializable {
+
+  type T <: Clusters
+
+  /* Update the clustering data structure, depending on the Instance given
+   *
+   * @param inst the Instance based on which the Model is updated
+   * @return the updated Clusters
+   */
+  def update(inst: Instance): T
+}
