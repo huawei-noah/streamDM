01c3e517d447929d19b5c827ec3539cc82726154
Silviu Maniu <silviu.maniu@gmail.com>
2015-04-15 17:34:09 +0800

    [ISSUE-17] Added distance functions in Instances.

--- src/test/scala/org/apache/spark/streamdm/core/SparseInstanceSuite.scala
+++ src/test/scala/org/apache/spark/streamdm/core/SparseInstanceSuite.scala
@@ -24,33 +24,33 @@ import org.scalatest.FunSuite
  */
 class SparseInstanceSuite extends FunSuite {
 
-  test("A SparseInstance should return its features given indices") {
+  test("return its features given indices") {
     val instance = SparseInstance(Array(0,1,2), Array(1.4,1.3,2.1))
     assert(instance(0) == 1.4)
     assert(instance(1) == 1.3)
     assert(instance(2) == 2.1)
     }
 
-  test("A SparseInstance should return 0.0 for a out of bounds index") {
+  test("return 0.0 for a out of bounds index") {
     val instance = SparseInstance(Array(0,2), Array(1.4,2.1))
     assert(instance(-1) == 0.0)
     assert(instance(1) == 0.0)
     assert(instance(3) == 0.0)
     }
 
-  test("A SparseInstance should have a dot operation with another SparseInstance") {
+  test("have a dot operation with another SparseInstance") {
     val instance1 = SparseInstance(Array(0,2), Array(1.4,2.1))
     val instance2 = SparseInstance(Array(1,2),Array(1.2,3.2))
     assert(instance1.dot(instance2) == 2.1*3.2)
   }
 
-  test("A SparseInstance should have a dot operation with a DenseInstance") {
+  test("have a dot operation with a DenseInstance") {
     val instance1 = SparseInstance(Array(0,2), Array(1.4,2.1))
     val instance2 = DenseInstance(Array(1.2,2.1,3.2))
     assert(instance1.dot(instance2) == 1.4*1.2+2.1*3.2)
   }
 
-  test("A SparseInstance should be able to add another SparseInstance") {
+  test("able to add another SparseInstance") {
     val instance1 = SparseInstance(Array(0,2), Array(1.4, 2.1))
     val instance2 = SparseInstance(Array(1,2), Array(1.6, 2.0))
     val sumInstance = instance1.add(instance2);
@@ -58,7 +58,7 @@ class SparseInstanceSuite extends FunSuite {
     sumInstance.indexes.map{case x => assert(sumInstance(x)==instance3(x))}
   }
 
-  test("A SparseInstance should be able to add a DenseInstance") {
+  test("able to add a DenseInstance") {
     val instance1 = SparseInstance(Array(0,2), Array(1.4, 2.1))
     val instance2 = DenseInstance(Array(0.0, 1.6, 2.0))
     val sumInstance = instance1.add(instance2);
@@ -66,20 +66,37 @@ class SparseInstanceSuite extends FunSuite {
     sumInstance.indexes.map{ case x => assert(sumInstance(x)==instance3(x))}
   }
 
-  test("A SparseInstance should be able to append a Feature") {
+  test("able to compute the distance to another SparseInstance") {
+    val instance1 = SparseInstance(Array(0,2), Array(1.4, 2.1))
+    val instance2 = SparseInstance(Array(1,2), Array(1.6, 2.0))
+    val dist = instance1.distanceTo(instance2);
+    val trueDist = math.sqrt(math.pow(1.4,2)+math.pow(1.6,2)+
+                   math.pow(2.1-2.0,2))
+    assert(dist==trueDist)
+  }
+
+  test("able to compute the distance to a DenseInstance") {
+    val instance1 = SparseInstance(Array(0,2), Array(1.4, 2.1))
+    val instance2 = DenseInstance(Array(0.0, 1.6, 2.0))
+    val sumInstance = instance1.add(instance2);
+    val instance3 = SparseInstance(Array(0,1,2), Array(1.4, 1.6, 2.1+2.0))
+    sumInstance.indexes.map{ case x => assert(sumInstance(x)==instance3(x))}
+  }
+
+  test("able to append a Feature") {
     val instance1 = SparseInstance(Array(1), Array(1.1))
     val instance2 = instance1.set(2,2.1)
     val instance3 = SparseInstance(Array(1,2), Array(1.1, 2.1))
     instance2.indexes.map{ case x => assert(instance2(x)==instance3(x))}
   }
 
-  test("A SparseInstance should have a map function for features") {
+  test("have a map function for features") {
     val instance1 = SparseInstance(Array(1,2), Array(1.4, 1.3))
     val instance2 = instance1.map(f=>f+2.0)
     instance2.indexes.map{ case x => assert(instance2(x)==instance1(x)+2.0)}
   }
 
-  test("A SparseInstance should have a parser in comma separated LibSVM format")
+  test("have a parser in comma separated LibSVM format")
   {
     val input = "1:1.1,3:2.1"
     val parsedInstance = SparseInstance.parse(input)
@@ -88,7 +105,7 @@ class SparseInstanceSuite extends FunSuite {
       assert(parsedInstance(x)==testInstance(x))}
   }
 
-  test("A SparseInstance should have a .toString override") {
+  test("have a .toString override") {
     val instance1 = SparseInstance(Array(1,2), Array(1.4, 1.3))
     assert(instance1.toString == "2:%f,3:%f".format(1.4,1.3))
   }
