eff3a31f4a7373e5524c096fefa0ca09795e778c
JianfengQian <Jianfeng.Qian@outlook.com>
2015-04-30 18:46:14 +0800

    add SplitCriterion

--- src/main/scala/org/apache/spark/streamdm/classifiers/trees/SplitCriterion.scala
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/SplitCriterion.scala
@@ -1,23 +1,29 @@
-package org.apache.spark.streamdm.classifier.trees
+package org.apache.spark.streamdm.classifiers.trees
 
 import scala.math.{ log, max }
 
+trait SplitCriterionType
+case class InfoGainSplitCriterionType() extends SplitCriterionType
+case class GiniSplitCriterionType() extends SplitCriterionType
+case class VarianceReductionSplitCriterionType() extends SplitCriterionType
+
 trait SplitCriterion extends Serializable {
-  def getMerit(pre: Array[Double], post: Array[Array[Double]]): Double
-  def getRangeMerit(pre: Array[Double]): Double
+  def merit(pre: Array[Double], post: Array[Array[Double]]): Double
+  def rangeMerit(pre: Array[Double]): Double
+  def negtive(pre: Array[Double]): Boolean = (pre.filter(x => x < 0).length > 0)
 }
 
 class InfoGainSplitCriterion(val minBranch: Double = 0.01) extends SplitCriterion with Serializable {
 
-  override def getMerit(pre: Array[Double], post: Array[Array[Double]]): Double = {
+  override def merit(pre: Array[Double], post: Array[Array[Double]]): Double = {
     if (numFrac(post, minBranch) < 2) Double.NegativeInfinity
     else entropy(pre) - entropy(post)
   }
 
-  override def getRangeMerit(pre: Array[Double]): Double = log(max(pre.length, 2))
+  override def rangeMerit(pre: Array[Double]): Double = log(max(pre.length, 2))
 
   def entropy(pre: Array[Double]): Double = {
-    if (pre == null || pre.sum <= 0) 0
+    if (pre == null || pre.sum <= 0 || negtive(pre)) 0.0
     log(pre.sum) - pre.filter(_ > 0).map(x => x * log(x)).sum / pre.sum
   }
 
@@ -25,15 +31,35 @@ class InfoGainSplitCriterion(val minBranch: Double = 0.01) extends SplitCriterio
     if (post == null || post.length == 0 || post(0).length == 0) 0
     val tpost: Array[Array[Double]] = Array.fill(post(0).length)(new Array[Double](post.length))
     post.zipWithIndex.map {
-      x => { x._1.zipWithIndex.map { y => tpost(x._2)(y._2) = post(y._2)(x._2) } }
+      x => { x._1.zipWithIndex.map { y => tpost(y._2)(x._2) = post(x._2)(y._2) } }
     }
     tpost.map { x => entropy(x) * x.sum }.sum / post.map(_.sum).sum
   }
 
   private[trees] def numFrac(post: Array[Array[Double]], minFrac: Double): Int = {
     if (post == null || post.length == 0) 0
-    val sums: Array[Double] = Array[Double](post(0).length)
-    post.map { x => (x.zipWithIndex.map { ix => sums(ix._2) += ix._1 }) }
+    val sums: Array[Double] = new Array[Double](post(0).length)
+    post.map { _.zipWithIndex.map { x => sums(x._2) += x._1 } }
     sums.filter(_ > sums.sum * minFrac).length
+  }
+}
+
+class GiniSplitCriterion extends SplitCriterion with Serializable {
+  //todo
+  override def merit(pre: Array[Double], post: Array[Array[Double]]): Double = 0
+  override def rangeMerit(pre: Array[Double]): Double = 0
+}
+class VarianceReductionSplitCriterion extends SplitCriterion with Serializable {
+  //todo
+  override def merit(pre: Array[Double], post: Array[Array[Double]]): Double = 0
+  override def rangeMerit(pre: Array[Double]): Double = 0
+}
+
+object SplitCriterion {
+  def createSplitCriterion(scType: SplitCriterionType, minBranch: Double = 0.01): SplitCriterion = scType match {
+    case infoGrain: InfoGainSplitCriterionType   => new InfoGainSplitCriterion(minBranch)
+    case gini: GiniSplitCriterionType            => new GiniSplitCriterion()
+    case vr: VarianceReductionSplitCriterionType => new VarianceReductionSplitCriterion()
+    case _                                       => new InfoGainSplitCriterion(minBranch)
   }
 }
