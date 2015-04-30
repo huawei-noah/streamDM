38b76939b9a80884501ffc4d25ec3008cad52cfe
JianfengQian <Jianfeng.Qian@outlook.com>
2015-04-30 17:16:34 +0800

    add FeatureSplit

---
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/FeatureSplit.scala
@@ -0,0 +1,12 @@
+package org.apache.spark.streamdm.classifiers.trees
+
+import scala.math.Ordered
+// Feature split suggestion
+class FeatureSplit(val conditionalTest: ConditionalTest, val merit: Double, val result: Array[Array[Double]]) extends Ordered[FeatureSplit] {
+  override def compare(that: FeatureSplit): Int = {
+    if (this.merit < that.merit) -1
+    else if (this.merit > that.merit) 1
+    else 0
+  }
+  def numSplit: Int = result.length
+}
