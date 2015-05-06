17604702b774a2111784019cd8690d00cd31cdf3
JianfengQian <Jianfeng.Qian@outlook.com>
2015-05-06 11:55:05 +0800

    add comments for ConditionalTest.scala

--- src/main/scala/org/apache/spark/streamdm/classifiers/trees/ConditionalTest.scala
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/ConditionalTest.scala
@@ -9,19 +9,47 @@ case class NominalBinaryTestType() extends ConditionalTestType
 case class NominalMultiwayTestType() extends ConditionalTestType
 case class NumericBinaryRulePredicateType() extends ConditionalTestType
 
+/**
+ * ConditionalTest is a condition test trait for examples
+ * to use to split nodes in Hoeffding trees.
+ */
 trait ConditionalTest extends Serializable {
+
+  /**
+   *  Returns the number of the branch for an example, -1 if unknown.
+   *
+   * @param example the Exmaple to be used
+   * @return the number of the branch for an example, -1 if unknown.
+   */
   def branch(example: Example): Int
+
+  /**
+   * Gets whether the number of the branch for an example is known.
+   *
+   * @param example Example
+   * @return true if the number of the branch for an example is known
+   */
   def hasResult(example: Example): Boolean = { branch(example) >= 0 }
+  /**
+   * Gets the number of maximum branches, -1 if unknown.
+   *
+   * @return the number of maximum branches, -1 if unknown..
+   */
   def maxBranches(): Int
 }
 
-case class NumericBinaryTest(val fIndex: Int, val value: Double, val isequalTest: Boolean) extends ConditionalTest with Serializable {
+/**
+ * Numeric binary conditional test for examples to use to split nodes in Hoeffding trees.
+ */
+
+case class NumericBinaryTest(val fIndex: Int, val value: Double, val isequalTest: Boolean)
+  extends ConditionalTest with Serializable {
 
   override def branch(example: Example): Int = {
     // todo process missing value
     val v = example.featureAt(fIndex)
-    if (v == value) {
-      if (isequalTest) 0 else 1
+    if (isequalTest) {
+      if (v == value) 0 else 1
     } else {
       if (v < value) 0 else 1
     }
@@ -29,8 +57,11 @@ case class NumericBinaryTest(val fIndex: Int, val value: Double, val isequalTest
 
   override def maxBranches(): Int = 2
 }
-
-case class NominalBinaryTest(val fIndex: Int, val value: Double) extends ConditionalTest with Serializable {
+/**
+ * Nominal binary conditional test for examples to use to split nodes in Hoeffding trees.
+ */
+case class NominalBinaryTest(val fIndex: Int, val value: Double)
+  extends ConditionalTest with Serializable {
 
   override def branch(example: Example): Int = {
     // todo process missing value
@@ -39,7 +70,9 @@ case class NominalBinaryTest(val fIndex: Int, val value: Double) extends Conditi
 
   override def maxBranches(): Int = 2
 }
-
+/**
+ * Nominal multi-way conditional test for examples to use to split nodes in Hoeffding trees.
+ */
 case class NominalMultiwayTest(val fIndex: Int) extends ConditionalTest with Serializable {
 
   override def branch(example: Example): Int = {
@@ -50,7 +83,12 @@ case class NominalMultiwayTest(val fIndex: Int) extends ConditionalTest with Ser
   override def maxBranches(): Int = -1
 }
 
-case class NumericBinaryRulePredicate(val fIndex: Int, val value: Double, val operator: Int) extends ConditionalTest with Serializable {
+/**
+ * Numeric binary rule predicate conditional test for
+ *  examples to use to split nodes in Hoeffding trees.
+ */
+case class NumericBinaryRulePredicate(val fIndex: Int, val value: Double, val operator: Int)
+  extends ConditionalTest with Serializable {
   // operator: 0 ==, 1 <=, 2 >
   override def branch(example: Example): Int = {
     // todo process missing value
