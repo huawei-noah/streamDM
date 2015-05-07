d5300228c74574959bf1a1b6f961eb05cae175e3
JianfengQian <Jianfeng.Qian@outlook.com>
2015-05-07 16:32:39 +0800

    add GaussianEstimatorSuite

---
+++ src/test/scala/org/apache/spark/streamdm/classifiers/trees/GaussianEstimatorSuite.scala
@@ -0,0 +1,55 @@
+package org.apache.spark.streamdm.classifiers.trees
+
+import org.scalatest.FunSuite
+class GaussianEstimatorSuite extends FunSuite {
+  test("test observe,stddev") {
+    val gs: GaussianEstimator = new GaussianEstimator()
+    gs.observe(2.0, 0.5)
+    assert(gs.getMean() == 2.0)
+    assert(gs.totalWeight() == 0.5)
+    gs.observe(1, 2)
+    assert(gs.totalWeight() == 2.5)
+    assert(gs.getMean() == 1.2)
+    assert(gs.stdDev() == 0.5163977794943222)
+    assert(gs.variance() == 0.2666666666666666)
+  }
+
+  test("test variance") {
+    val gs: GaussianEstimator = new GaussianEstimator()
+    gs.observe(2.0, 0.5)
+    assert(gs.variance() == 0)
+    gs.observe(1, 0.5)
+    assert(gs.variance() == 0)
+    gs.observe(3, 0.5)
+    assert(gs.variance() == 2.0)
+  }
+  test("test probalilityDensity") {
+    val gs: GaussianEstimator = new GaussianEstimator()
+    assert(gs.probabilityDensity(0) == 0)
+    assert(gs.probabilityDensity(1) == 0)
+    assert(gs.probabilityDensity(2) == 0)
+    gs.observe(2.0, 1)
+    assert(gs.probabilityDensity(0) == 0)
+    assert(gs.probabilityDensity(1) == 0)
+    assert(gs.probabilityDensity(2) == 1)
+  }
+  test("test merge") {
+    val gs1: GaussianEstimator = new GaussianEstimator()
+    gs1.observe(2.0, 1)
+    gs1.observe(1.0, 2.0)
+    val gs2: GaussianEstimator = new GaussianEstimator()
+    gs2.observe(3.0, 1)
+    gs2.observe(1.0, 3.0)
+    val merge: GaussianEstimator = gs1.merge(gs2);
+    val gs3: GaussianEstimator = new GaussianEstimator()
+    gs3.observe(2.0, 1)
+    gs3.observe(1.0, 2.0)
+    gs3.observe(3.0, 1)
+    gs3.observe(1.0, 3.0)
+    assert(merge.getMean() == gs3.getMean())
+    assert(merge.totalWeight() ==gs3.totalWeight())
+    assert(merge.stdDev() == merge.stdDev())
+    assert(merge.variance() ==merge.variance())
+  }
+
+}
