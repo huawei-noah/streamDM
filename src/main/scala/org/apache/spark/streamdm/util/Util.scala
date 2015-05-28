84727c01df7430aadb5a9a48c0a1abbd5ee5f144
JianfengQian <Jianfeng.Qian@outlook.com>
2015-05-28 20:03:31 +0800

    add matrix & array to string fuction and log2

--- src/main/scala/org/apache/spark/streamdm/util/Util.scala
+++ src/main/scala/org/apache/spark/streamdm/util/Util.scala
@@ -1,10 +1,12 @@
 package org.apache.spark.streamdm.util
 
-import scala.math.{ min }
+import scala.math.{ min, max, log }
 
 object Util {
 
   def argmax(array: Array[Double]): Double = array.zipWithIndex.maxBy(_._1)._2
+
+  def log2(v: Double): Double = log(v) / log(2)
 
   def transpose(input: Array[Array[Double]]): Array[Array[Double]] = {
     val output: Array[Array[Double]] = Array.fill(input(0).length)(new Array[Double](input.length))
@@ -34,6 +36,25 @@ object Util {
     for (i <- 0 until merge.length)
       merge(i) = array1(i) + array2(i)
     merge
+  }
+
+  def matrixtoString(post: Array[Array[Double]]): String = {
+    val sb = new StringBuffer("\n")
+    for (i <- 0 until post.length) {
+      for (j <- 0 until post(i).length) {
+        sb.append(post(i)(j) + "\t")
+      }
+      sb.append("\n")
+    }
+    sb.substring(0, sb.length() - 1)
+  }
+
+  def arraytoString(pre: Array[Double]): String = {
+    val sb = new StringBuffer("\n")
+    for (i <- 0 until pre.length) {
+      sb.append(pre(i) + "\t")
+    }
+    sb.substring(0, sb.length() - 1)
   }
 
   def main(args: Array[String]) = {
