4bcb82bf44990c3a340d2d71d2182e284a8c272d
JianfengQian <Jianfeng.Qian@outlook.com>
2015-05-30 10:03:05 +0800

    add comments

--- src/main/scala/org/apache/spark/streamdm/classifiers/trees/FeatureSplit.scala
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/FeatureSplit.scala
@@ -1,7 +1,27 @@
+/*
+ * Copyright (C) 2015 Holmes Team at HUAWEI Noah's Ark Lab.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.spark.streamdm.classifiers.trees
 
 import scala.math.Ordered
-// Feature split suggestion
+
+/**
+ * class FeatureSplit is a split suggestion
+ */
 class FeatureSplit(val conditionalTest: ConditionalTest, val merit: Double, val result: Array[Array[Double]]) extends Ordered[FeatureSplit] {
   override def compare(that: FeatureSplit): Int = {
     if (this.merit < that.merit) -1
@@ -11,5 +31,7 @@ class FeatureSplit(val conditionalTest: ConditionalTest, val merit: Double, val 
   def numSplit: Int = result.length
 
   def distributionFromSplit(splitIndex: Int): Array[Double] = result(splitIndex)
+
+  override def toString(): String = "FeatureSplit, merit=" + merit + ", " + conditionalTest
 
 }
