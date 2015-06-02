87517e88f98ed8004fa816666cc2f804f04b2ab4
JianfengQian <Jianfeng.Qian@outlook.com>
2015-06-02 13:49:58 +0800

    update testsuite

--- src/test/scala/org/apache/spark/streamdm/classifiers/trees/ConditionalTestSuite.scala
+++ src/test/scala/org/apache/spark/streamdm/classifiers/trees/ConditionalTestSuite.scala
@@ -26,21 +26,21 @@ class ConditionalTestSuite extends FunSuite {
     val example0: Example = new Example(new DenseInstance(Array[Double](0, 5, 5)), null)
     val example1: Example = new Example(new DenseInstance(Array[Double](1, 5, 5)), null)
     val example2: Example = new Example(new DenseInstance(Array[Double](2, 5, 5)), null)
-    assert(nb1.branch(example0) == 0)
-    assert(nb1.branch(example1) == 1)
+    assert(nb1.branch(example0) == 1)
+    assert(nb1.branch(example1) == 0)
     assert(nb1.branch(example2) == 1)
     assert(nb1.maxBranches() == 2)
   }
 
   test("test NominalMultiwayTest") {
-    val nb1: NominalMultiwayTest = new NominalMultiwayTest(0)
+    val nb1: NominalMultiwayTest = new NominalMultiwayTest(0, 3)
     val example0: Example = new Example(new DenseInstance(Array[Double](0, 5, 5)), null)
     val example1: Example = new Example(new DenseInstance(Array[Double](1, 5, 5)), null)
     val example2: Example = new Example(new DenseInstance(Array[Double](2, 5, 5)), null)
     assert(nb1.branch(example0) == 0)
     assert(nb1.branch(example1) == 1)
     assert(nb1.branch(example2) == 2)
-    assert(nb1.maxBranches() == -1)
+    assert(nb1.maxBranches() == 3)
   }
 
   test("test NumericBinaryRulePredicate") {
@@ -48,7 +48,7 @@ class ConditionalTestSuite extends FunSuite {
     val nb1: NumericBinaryRulePredicate = new NumericBinaryRulePredicate(0, 1, 0)
     //nb2 <=
     val nb2: NumericBinaryRulePredicate = new NumericBinaryRulePredicate(0, 1, 1)
-    //nb3 >
+    //nb3 <
     val nb3: NumericBinaryRulePredicate = new NumericBinaryRulePredicate(0, 1, 2)
     val example0: Example = new Example(new DenseInstance(Array[Double](0, 5, 5)), null)
     val example1: Example = new Example(new DenseInstance(Array[Double](1, 5, 5)), null)
@@ -59,9 +59,9 @@ class ConditionalTestSuite extends FunSuite {
     assert(nb2.branch(example0) == 0)
     assert(nb2.branch(example1) == 0)
     assert(nb2.branch(example2) == 1)
-    assert(nb3.branch(example0) == 1)
+    assert(nb3.branch(example0) == 0)
     assert(nb3.branch(example1) == 1)
-    assert(nb3.branch(example2) == 0)
+    assert(nb3.branch(example2) == 1)
     assert(nb2.maxBranches() == 2)
   }
 }
--- src/test/scala/org/apache/spark/streamdm/classifiers/trees/GaussianEstimatorSuite.scala
+++ src/test/scala/org/apache/spark/streamdm/classifiers/trees/GaussianEstimatorSuite.scala
@@ -1,29 +1,29 @@
 package org.apache.spark.streamdm.classifiers.trees
 
 import org.scalatest.FunSuite
-import scala.math.{abs}
+import scala.math.{ abs }
 class GaussianEstimatorSuite extends FunSuite {
 
-  test("test observe,stddev") {
+  test("test observe,stddev,variance,merge") {
     val gs: GaussianEstimator = new GaussianEstimator()
     gs.observe(2.0, 0.5)
-    assert(gs.getMean() == 2.0)
-    assert(gs.totalWeight() == 0.5)
     gs.observe(1, 2)
-    assert(gs.totalWeight() == 2.5)
-    assert(gs.getMean() == 1.2)
-    assert(gs.stdDev() == 0.5163977794943222)
-    assert(gs.variance() == 0.2666666666666666)
+    var gs2: GaussianEstimator = new GaussianEstimator()
+    gs2 = gs2.merge(gs, false)
+    assert(gs2.totalWeight() == 2.5)
+    assert(gs2.getMean() == 1.2)
+    assert(gs2.stdDev() == 0.5163977794943222)
+    assert(gs2.variance() == 0.2666666666666666)
   }
 
   test("test variance") {
     val gs: GaussianEstimator = new GaussianEstimator()
     gs.observe(2.0, 0.5)
-    assert(gs.variance() == 0)
     gs.observe(1, 0.5)
-    assert(gs.variance() == 0)
     gs.observe(3, 0.5)
-    assert(gs.variance() == 2.0)
+    var gs2: GaussianEstimator = new GaussianEstimator()
+    gs2 = gs2.merge(gs, false)
+    assert(gs2.variance() == 2.0)
   }
 
   test("test probalilityDensity") {
@@ -32,32 +32,11 @@ class GaussianEstimatorSuite extends FunSuite {
     assert(gs.probabilityDensity(1) == 0)
     assert(gs.probabilityDensity(2) == 0)
     gs.observe(2.0, 1)
-    assert(gs.probabilityDensity(0) == 0)
-    assert(gs.probabilityDensity(1) == 0)
-    assert(gs.probabilityDensity(2) == 1)
-    gs.observe(1, 2)
-    assert(gs.probabilityDensity(gs.getMean()) == 0.690988298942671)
-    assert(gs.probabilityDensity(1) == 0.5849089671682234)
-    assert(abs(gs.probabilityDensity(2) -0.3547652217283777)<0.0000001)
-  }
-
-  test("test merge") {
-    val gs1: GaussianEstimator = new GaussianEstimator()
-    gs1.observe(2.0, 1)
-    gs1.observe(1.0, 2.0)
-    val gs2: GaussianEstimator = new GaussianEstimator()
-    gs2.observe(3.0, 1)
-    gs2.observe(1.0, 3.0)
-    val merge: GaussianEstimator = gs1.merge(gs2);
-    val gs3: GaussianEstimator = new GaussianEstimator()
-    gs3.observe(2.0, 1)
-    gs3.observe(1.0, 2.0)
-    gs3.observe(3.0, 1)
-    gs3.observe(1.0, 3.0)
-    assert(merge.getMean() == gs3.getMean())
-    assert(merge.totalWeight() == gs3.totalWeight())
-    assert(merge.stdDev() == merge.stdDev())
-    assert(merge.variance() == merge.variance())
+    var gs2: GaussianEstimator = new GaussianEstimator()
+    gs2 = gs2.merge(gs, false)
+    assert(gs2.probabilityDensity(0) == 0)
+    assert(gs2.probabilityDensity(1) == 0)
+    assert(gs2.probabilityDensity(2) == 1)
   }
 
 }
