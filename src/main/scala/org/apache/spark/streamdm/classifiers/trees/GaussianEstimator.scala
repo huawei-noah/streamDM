a2f3863231def120fcd9adb481c3d67051288820
JianfengQian <Jianfeng.Qian@outlook.com>
2015-06-04 19:35:57 +0800

    change the update mode, merge(,false) will be stored at block_

--- src/main/scala/org/apache/spark/streamdm/classifiers/trees/GaussianEstimator.scala
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/GaussianEstimator.scala
@@ -25,8 +25,8 @@ import org.apache.spark.streamdm.util.Statistics
  * for more info see Donald Knuth's "The Art of Computer Programming, Volume 2: Seminumerical Algorithms", section 4.2.2.
  */
 
-class GaussianEstimator(val weightSum: Double = 0.0, val mean: Double = 0.0,
-                        val varianceSum: Double = 0.0) extends Serializable {
+class GaussianEstimator(var weightSum: Double = 0.0, var mean: Double = 0.0,
+                        var varianceSum: Double = 0.0) extends Serializable {
   val normal_constant: Double = sqrt(2 * Pi)
   var blockWeightSum: Double = 0.0
   var blockMean: Double = 0.0
@@ -56,26 +56,35 @@ class GaussianEstimator(val weightSum: Double = 0.0, val mean: Double = 0.0,
    */
   def merge(that: GaussianEstimator, trySplit: Boolean): GaussianEstimator = {
     if (!trySplit) {
-      if (this.weightSum == 0)
-        new GaussianEstimator(that.blockWeightSum, that.blockMean, that.blockVarianceSum)
-      else {
+      if (this.blockWeightSum == 0) {
+        blockWeightSum = that.blockWeightSum
+        blockMean = that.blockMean
+        blockVarianceSum = that.blockVarianceSum
+      } else {
+        val newBlockWeightSum = blockWeightSum + that.blockWeightSum
+        val newBlockMean = (this.blockMean * blockWeightSum + that.blockMean * that.blockWeightSum) / newBlockWeightSum
+        val newBlockVarianceSum = this.blockVarianceSum + that.blockVarianceSum + pow(this.blockMean - that.blockMean, 2) *
+          this.blockWeightSum * that.blockWeightSum / (this.blockWeightSum + that.blockWeightSum)
+        blockWeightSum = newBlockWeightSum
+        blockMean = newBlockMean
+        blockVarianceSum = newBlockVarianceSum
+      }
+    } else {
+      if (this.weightSum == 0) {
+        weightSum = that.blockWeightSum
+        mean = that.blockMean
+        varianceSum = that.blockVarianceSum
+      } else {
         val newWeightSum = weightSum + that.blockWeightSum
         val newMean = (this.mean * weightSum + that.blockMean * that.blockWeightSum) / newWeightSum
         val newVarianceSum = this.varianceSum + that.blockVarianceSum + pow(this.mean - that.blockMean, 2) *
           this.weightSum * that.blockWeightSum / (this.weightSum + that.blockWeightSum)
-        new GaussianEstimator(newWeightSum, newMean, newVarianceSum)
-      }
-    } else {
-      if (this.weightSum == 0)
-        new GaussianEstimator(that.weightSum, that.mean, that.varianceSum)
-      else {
-        val newWeightSum = weightSum + that.weightSum
-        val newMean = (this.mean * weightSum + that.mean * that.weightSum) / newWeightSum
-        val newVarianceSum = this.varianceSum + that.varianceSum + pow(this.mean - that.mean, 2) *
-          this.weightSum * that.weightSum / (this.weightSum + that.weightSum)
-        new GaussianEstimator(newWeightSum, newMean, newVarianceSum)
+        weightSum = newWeightSum
+        mean = newMean
+        varianceSum = newVarianceSum
       }
     }
+    this
   }
 
   def totalWeight(): Double = {
