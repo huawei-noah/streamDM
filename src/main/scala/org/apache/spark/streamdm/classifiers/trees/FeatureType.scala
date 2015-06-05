8d0894d17002dc9c951ed124962cf2cab4a1f5ed
JianfengQian <Jianfeng.Qian@outlook.com>
2015-06-05 15:07:46 +0800

    update FeatureType

--- src/main/scala/org/apache/spark/streamdm/classifiers/trees/FeatureType.scala
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/FeatureType.scala
@@ -15,20 +15,20 @@
  *
  */
 
-package org.apache.spark.streamdm.classifiers.trees
-
-trait FeatureType {
-  def getRange(): Int = 0
-}
-
-case class NullFeatureType() extends FeatureType with Serializable
-
-case class NominalFeatureType(val range: Int) extends FeatureType with Serializable {
-  override def getRange(): Int = range
-}
-
-case class NumericFeatureType() extends FeatureType with Serializable
-
-class FeatureTypeArray(val featureTypes: Array[FeatureType]) extends Serializable {
-  def numFeatures() = if (featureTypes == null) 0 else featureTypes.length
-}
+//package org.apache.spark.streamdm.classifiers.trees
+//
+//trait FeatureType {
+//  def getRange(): Int = 0
+//}
+//
+//case class NullFeatureType() extends FeatureType with Serializable
+//
+//case class NominalFeatureType(val range: Int) extends FeatureType with Serializable {
+//  override def getRange(): Int = range
+//}
+//
+//case class NumericFeatureType() extends FeatureType with Serializable
+//
+//class FeatureTypeArray(val featureTypes: Array[FeatureType]) extends Serializable {
+//  def numFeatures() = if (featureTypes == null) 0 else featureTypes.length
+//}
