4ada7bee722bb5f3b0aaa2a036c778384c1befbf
JianfengQian <Jianfeng.Qian@outlook.com>
2015-06-09 16:06:16 +0800

    tmp file to evalute HoeffdingTree

---
+++ src/main/scala/org/apache/spark/streamdm/tasks/EvaluateHoeffdingTree.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 Holmes Team at HUAWEI Noah's Ark Lab.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.spark.streamdm.tasks
+
+import com.github.javacliparser.ClassOption
+import org.apache.spark.streamdm.core._
+import org.apache.spark.streamdm.classifiers._
+import org.apache.spark.streamdm.streams._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streamdm.evaluation.Evaluator
+
+/**
+ * Task for evaluating a classifier on a stream by testing then training with each example in sequence.
+ *
+ */
+class EvaluateHoeffdingTree extends Task {
+
+  //  val learnerOption: ClassOption = new ClassOption("learner", 'l',
+  //    "Learner to use", classOf[Classifier], "bayes.MultinomialNaiveBayes")
+
+  val learnerOption: ClassOption = new ClassOption("learner", 'l',
+    "Learner to use", classOf[Classifier], "trees.HoeffdingTree")
+
+  //    val learnerOption: ClassOption = new ClassOption("learner", 'l',
+  //    "Learner to use", classOf[Classifier], "trees.IncrementalClassfier")
+
+  //  val learnerOptionbak: ClassOption = new ClassOption("learner", 'l',
+  //    "Learner to use", classOf[Classifier], "SGDLearner")
+
+  val evaluatorOption: ClassOption = new ClassOption("evaluator", 'e',
+    "Evaluator to use", classOf[Evaluator], "BasicClassificationEvaluator")
+
+  val streamReaderOption: ClassOption = new ClassOption("streamReader", 's',
+    "Stream reader to use", classOf[StreamReader], "generators.RandomTreeGenerator")
+
+  val resultsWriterOption: ClassOption = new ClassOption("resultsWriter", 'w',
+    "Stream writer to use", classOf[StreamWriter], "PrintStreamWriter")
+
+  def run(ssc: StreamingContext): Unit = {
+    val reader: StreamReader = this.streamReaderOption.getValue()
+
+    val learner: Classifier = this.learnerOption.getValue()
+    learner.init(reader.getExampleSpecification())
+
+    val evaluator: Evaluator = this.evaluatorOption.getValue()
+
+    val writer: StreamWriter = this.resultsWriterOption.getValue()
+
+    val instances = reader.getExamples(ssc)
+
+    //Predict
+    val predPairs = learner.predict(instances)
+
+    //Train
+    learner.train(instances)
+
+    //Evaluate
+    writer.output(evaluator.addResult(predPairs))
+
+  }
+}
