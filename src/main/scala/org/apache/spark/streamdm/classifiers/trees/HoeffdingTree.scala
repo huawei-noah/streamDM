0b5e898832334a16989f5e168927fce844ba5b33
JianfengQian <Jianfeng.Qian@outlook.com>
2015-06-29 11:57:20 +0800

    update the description of Hoeffding Tree

--- src/main/scala/org/apache/spark/streamdm/classifiers/trees/HoeffdingTree.scala
+++ src/main/scala/org/apache/spark/streamdm/classifiers/trees/HoeffdingTree.scala
@@ -36,38 +36,19 @@ import org.apache.spark.streamdm.classifiers._
  * Hoeffding Tree or VFDT.
  *
  *
+ * The Hoeffding tree is an incremental decision tree learner for large data streams,
+ * that assumes that the data distribution is not changing over time.
+ * It grows incrementally a decision tree based on the theoretical guarantees of the Hoeffding bound (or additive Chernoff bound).
+ * A node is expanded, as soon as there is sufficient statistical evidence that an optimal splitting feature exists,
+ * and this decision is based on the distribution-independent Hoeffding bound.
+ * The model learned by the Hoeffding tree is asymptotically nearly identical to the one built by a non-incremental learner,
+ * if the number of training instances is large enough. See for details:
  *
- * A Hoeffding tree is an incremental, anytime decision tree induction algorithm
- *
- * that is capable of learning from massive data streams, assuming that the
- *
- * distribution generating examples does not change over time. Hoeffding trees
- *
- * exploit the fact that a small sample can often be enough to choose an optimal
- *
- * splitting attribute. This idea is supported mathematically by the Hoeffding
- *
- * bound, which quantiﬁes the number of observations (in our case, examples)
- *
- * needed to estimate some statistics within a prescribed precision (in our
- *
- * case, the goodness of a feature).A theoretically appealing feature
- *
- * of Hoeffding Trees not shared by other incremental decision tree learners is
- *
- * that it has sound guarantees of performance. Using the Hoeffding bound one
- *
- * can show that its output is asymptotically nearly identical to that of a
- *
- * non-incremental learner using inﬁnitely many examples. See for details:
- *
- *
+ * P. Domingos and G. Hulten. Mining High-Speed Data Streams.
+ * In KDD'00, pages 71-80, Boston, MA, 2000. ACM Press.
  *
  * G. Hulten, L. Spencer, and P. Domingos. Mining time-changing data streams.
- *
  * In KDD’01, pages 97–106, San Francisco, CA, 2001. ACM Press.
- *
- *
  *
  * Parameters:
  * -n : Numeric Observer to use : only support Gaussian approximation by now.
