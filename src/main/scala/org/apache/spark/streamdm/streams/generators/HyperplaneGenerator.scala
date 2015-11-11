ced85d9ab04cfff8f7b5c23cb4c46503a19d3af6
JianfengQian <Jianfeng.Qian@outlook.com>
2015-11-11 20:01:54 +0800

    update HyperplaneGenerator to new superclass generator

--- src/main/scala/org/apache/spark/streamdm/streams/generators/HyperplaneGenerator.scala
+++ src/main/scala/org/apache/spark/streamdm/streams/generators/HyperplaneGenerator.scala
@@ -20,26 +20,25 @@ import com.github.javacliparser.IntOption
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streamdm.core._
 import org.apache.spark.streamdm.streams.StreamReader
-import org.apache.spark.streaming.{Duration, Time, StreamingContext}
-import org.apache.spark.streaming.dstream.{InputDStream, DStream}
+import org.apache.spark.streaming.{ Duration, Time, StreamingContext }
+import org.apache.spark.streaming.dstream.{ InputDStream, DStream }
 
 import scala.util.Random
-
 
 /**
  * Stream generator for generating data from a hyperplane.
  *
  * <p>It uses the following options:
  * <ul>
- *  <li> Chunk size (<b>-c</b>)
+ *  <li> Chunk size (<b>-k</b>)
  *  <li> Slide duration in milliseconds (<b>-d</b>)
  *  <li> Number of features (<b>-f</b>)
  * </ul>
  */
 
-class HyperplaneGenerator extends StreamReader {
+class HyperplaneGenerator extends Generator {
 
-  val chunkSizeOption: IntOption = new IntOption("chunkSize", 'c',
+  val chunkSizeOption: IntOption = new IntOption("chunkSize", 'k',
     "Chunk Size", 1000, 1, Integer.MAX_VALUE)
 
   val slideDurationOption: IntOption = new IntOption("slideDuration", 'd',
@@ -49,55 +48,44 @@ class HyperplaneGenerator extends StreamReader {
     "Number of Features", 3, 1, Integer.MAX_VALUE)
 
   /**
-   * Obtains a stream of random examples.
-   *
-   * @param ssc a Spark Streaming context
-   * @return a stream of Examples
+   * returns chunk size
    */
-  def getExamples(ssc:StreamingContext): DStream[Example] = {
-    new InputDStream[Example](ssc){
+  override def getChunkSize(): Int = {
+    chunkSizeOption.getValue
+  }
 
-      override def start(): Unit = {}
-
-      override def stop(): Unit = {}
-
-      override def compute(validTime: Time): Option[RDD[Example]] = {
-        val examples:Array[Example] = Array.fill[Example](chunkSizeOption.
-              getValue)(getExample)
-          Some(ssc.sparkContext.parallelize(examples))
-      }
-
-      override def slideDuration = {
-        new Duration(slideDurationOption.getValue)
-      }
-
-      def getExample(): Example = {
-        val inputInstance = new DenseInstance(Array.fill[Double](
-            numFeaturesOption.getValue)(5.0 * getRandomNumber()))
-        val noiseInstance = new DenseInstance(Array.fill[Double](
-            numFeaturesOption.getValue)(getNoise()))
-        new Example(inputInstance.add(noiseInstance), new DenseInstance(
-            Array.fill[Double](1)(label(inputInstance))))
-      }
-
-      def getRandomNumber():Double = 2.0 * Random.nextDouble() - 1.0 
-
-      def getNoise():Double = 0.5 * Random.nextGaussian()
-
-      val weight = new DenseInstance(Array.fill[Double](
-          numFeaturesOption.getValue)(getRandomNumber()))
-
-      val bias:Double = getRandomNumber()
-
-      def label(inputInstance: Instance):Double = {
-        val sum = weight.dot(inputInstance)
-        if (sum > bias) 1
-          else 0
-      }
-    }
+  /**
+   * returns slide duration
+   */
+  override def getslideDuration(): Int = {
+    slideDurationOption.getValue
   }
 
   def init(): Unit = {}
+
+  def getExample(): Example = {
+    val inputInstance = new DenseInstance(Array.fill[Double](
+      numFeaturesOption.getValue)(5.0 * getRandomNumber()))
+    val noiseInstance = new DenseInstance(Array.fill[Double](
+      numFeaturesOption.getValue)(getNoise()))
+    new Example(inputInstance.add(noiseInstance), new DenseInstance(
+      Array.fill[Double](1)(label(inputInstance))))
+  }
+
+  def getRandomNumber(): Double = 2.0 * Random.nextDouble() - 1.0
+
+  def getNoise(): Double = 0.5 * Random.nextGaussian()
+
+  val weight = new DenseInstance(Array.fill[Double](
+    numFeaturesOption.getValue)(getRandomNumber()))
+
+  val bias: Double = getRandomNumber()
+
+  def label(inputInstance: Instance): Double = {
+    val sum = weight.dot(inputInstance)
+    if (sum > bias) 1
+    else 0
+  }
 
   /**
    * Obtains the specification of the examples in the stream.
@@ -108,7 +96,7 @@ class HyperplaneGenerator extends StreamReader {
 
     //Prepare specification of class attributes
     val outputIS = new InstanceSpecification()
-    val classFeature = new NominalFeatureSpecification(Array("+","-"))
+    val classFeature = new NominalFeatureSpecification(Array("+", "-"))
     outputIS.setFeatureSpecification(0, classFeature)
     outputIS.setName(0, "class")
 
