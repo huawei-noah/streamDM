d5ac6489b0c1bdc3f89ce5c80e934145b120067b
JianfengQian <Jianfeng.Qian@outlook.com>
2015-11-15 12:37:34 +0800

    update sparks.sh

--- scripts/spark.sh
+++ scripts/spark.sh
@@ -1,8 +1,8 @@
-SPARK_HOME=/home/albert/Software/spark/spark-1.1.1
+SPARK_HOME=/home/jianfeng/scala/spark-1.3.0-bin-hadoop2.4
 $SPARK_HOME/bin/spark-submit \
   --class "org.apache.spark.streamdm.streamDMJob" \
   --master local[2] \
-  ../target/scala-2.10/streamdm-spark-streaming-_2.10-0.1.jar \
+  /home/jianfeng/git/streamDM/target/scala-2.10/streamdm-spark-streaming-_2.10-0.1.jar \
   $1
 
 
