e08f517aafdd04ca95e83e6bb2a7cc981184279b
JianfengQian <Jianfeng.Qian@outlook.com>
2015-12-30 19:10:31 +0800

    remove two function:addInputFeatureSpecification and add OutputFeatureSpecification

--- src/main/scala/org/apache/spark/streamdm/core/specification/ExampleSpecification.scala
+++ src/main/scala/org/apache/spark/streamdm/core/specification/ExampleSpecification.scala
@@ -15,7 +15,6 @@
  *
  */
 
-
 package org.apache.spark.streamdm.core.specification
 
 /**
@@ -23,85 +22,77 @@ package org.apache.spark.streamdm.core.specification
  * features.  It contains a reference to an input InstanceSpecification and an
  * output InstanceSpecification, and provides setters and getters for the
  * feature specification properties.
-*/
+ */
 
 class ExampleSpecification(inInstanceSpecification: InstanceSpecification,
-                           outInstanceSpecification: InstanceSpecification)
-  extends Serializable {
+  outInstanceSpecification: InstanceSpecification)
+    extends Serializable {
 
   val in = inInstanceSpecification
   val out = outInstanceSpecification
 
-  /** Gets the input FeatureSpecification value present at position index
-    *
-    * @param index the index of the specification 
-    * @return a FeatureSpecification representing the specification for the
-    * feature
-    */
+  /**
+   * Gets the input FeatureSpecification value present at position index
+   *
+   * @param index the index of the specification
+   * @return a FeatureSpecification representing the specification for the
+   * feature
+   */
   def inputFeatureSpecification(index: Int): FeatureSpecification = in(index)
 
-  /** Gets the output FeatureSpecification value present at position index
-    *
-    * @param index the index of the specification
-    * @return a FeatureSpecification representing the specification for the
-    * feature
-    */
-  def outputFeatureSpecification(index: Int): FeatureSpecification  = out(index)
+  /**
+   * Gets the output FeatureSpecification value present at position index
+   *
+   * @param index the index of the specification
+   * @return a FeatureSpecification representing the specification for the
+   * feature
+   */
+  def outputFeatureSpecification(index: Int): FeatureSpecification = out(index)
 
-  /** Adds a specification of an input instance feature
-    *
-    * @param index the index at which the value is added
-    * @param input the feature specification which is added up
-    */
-  def setInputFeatureSpecification(index:Int, input:FeatureSpecification):Unit =
-    in.setFeatureSpecification(index, input)
+  /**
+   * Evaluates whether an input feature is numeric
+   *
+   * @param index the index of the feature
+   * @return true if the feature is numeric
+   */
+  def isNumericInputFeature(index: Int): Boolean = in.isNumeric(index)
 
-  /** Adds a specification of an output instance feature
-    *
-    * @param index the index at which the value is added
-    * @param input the feature specification which is added up
-    */
-  def setOutputFeatureSpecification(index:Int, input:FeatureSpecification):Unit=
-    out.setFeatureSpecification(index, input)
+  /**
+   * Evaluates whether an output feature is numeric
+   *
+   * @param index the index of the feature
+   * @return true if the feature is numeric
+   */
+  def isNumericOutputFeature(index: Int): Boolean = out.isNumeric(index)
 
-  /** Evaluates whether an input feature is numeric
-    *
-    * @param index the index of the feature
-    * @return true if the feature is numeric
-    */
-  def isNumericInputFeature(index:Int):Boolean = in.isNumeric(index)
+  /**
+   * Gets the input name of the feature at position index
+   *
+   * @param index the index of the class
+   * @return a string representing the name of the feature
+   */
+  def nameInputFeature(index: Int): String = in.name(index)
 
-  /** Evaluates whether an output feature is numeric
-    *
-    * @param index the index of the feature
-    * @return true if the feature is numeric
-    */
-  def isNumericOutputFeature(index:Int):Boolean = out.isNumeric(index)
+  /**
+   * Gets the output name of the feature at position index
+   *
+   * @param index the index of the class
+   * @return a string representing the name of the feature
+   */
+  def nameOutputFeature(index: Int): String = out.name(index)
 
-  /** Gets the input name of the feature at position index
-    *
-    * @param index the index of the class
-    * @return a string representing the name of the feature
-    */
-  def nameInputFeature(index:Int):String = in.name(index)
-
-  /** Gets the output name of the feature at position index
-    *
-    * @param index the index of the class
-    * @return a string representing the name of the feature
-    */
-  def nameOutputFeature(index:Int):String = out.name(index)
-
-  /** Gets the number of input features
-    *
-    * @return an Integer representing the number of input features
-    */
+  /**
+   * Gets the number of input features
+   *
+   * @return an Integer representing the number of input features
+   */
   def numberInputFeatures(): Int = in.size
 
-  /** Gets the number of output features
-    *
-    * @return an Integer representing the number of output features
-    */
+  /**
+   * Gets the number of output features
+   *
+   * @return an Integer representing the number of output features
+   */
   def numberOutputFeatures(): Int = out.size
 }
 
