136868a037fba5996ccf262ea7dd7615d15a073d
Jianfeng Qian <Jianfeng.Qian@outlook.com>
2016-01-14 16:30:05 +0800

    set the flexiable version

--- scripts/generateData.sh
+++ scripts/generateData.sh
@@ -1,1 +1,1 @@
-java -cp ../target/scala-2.10/streamdm-spark-streaming-_2.10-0.1.jar:$SPARK_HOME/lib/spark-assembly-1.3.0-hadoop2.4.0.jar org.apache.spark.streamdm.streams.SampleDataWriter $1
+java -cp ../target/scala-2.10/streamdm-spark-streaming-_2.10-0.*.jar:$SPARK_HOME/lib/spark-assembly-1.*.0-hadoop2.*.0.jar org.apache.spark.streamdm.streams.SampleDataWriter $1
